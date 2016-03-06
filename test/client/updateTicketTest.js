996751196e7b72c31608781b12ab5e430a8b2e4b
Zenedith <github@zenedith.mailgun.org>
2016-03-06 21:42:47 +0100

    Handle special tickets in update and get

--- test/client/updateTicketTest.js
+++ test/client/updateTicketTest.js
@@ -1,27 +1,43 @@
-var options = require('config');
-var updateTicketClient = require('../../lib/client/updateTicket');
-var authClient = require('../../lib/client/auth');
-var chai = require('chai');
+var options = require("config");
+var updateTicketClient = require("../../lib/client/updateTicket");
+var authClient = require("../../lib/client/auth");
+var chai = require("chai");
 var should = chai.should();
 var expect = chai.expect;
 
-describe('update ticket client test', function () {
+describe("update ticket client test", function () {
 
-  it('should update lottery ticket', function (done) {
+  it("should update lottery ticket", function (done) {
+
+    var id = "56db6006142877ae4d8bd1d5";
 
     var updateTicketRequest = {
-      pointOfSale: "bds12160186",
-      purchaseOrderNumber: "78957",
-      taxRegistrationNumber: "5260211104",
-      date: "2016-02-01T00:00:00Z",
-      amount: {
-        value: "19.95",
-        currency: "PLN"
-      },
-      trade: ""
+      id: "56db6006142877ae4d8bd1d5",
+      code: "FUP72DM1EAY",
+      purchaseOrderNumber: "096750",
+      amount: {currency: "PLN", value: "77.90"},
+      date: "2016-03-04T23:00:00.798Z",
+      special: false,
+      pointOfSale: "bae13114086",
+      taxRegistrationNumber: "5260207427",
+      trade: "OTHER"
     };
 
-    var id = '56af34a214287718788b53d3';
+
+    //var id = "563e50c6d323774c0b8b5a97";
+    //
+    //var updateTicketRequest =
+    //{
+    //  purchaseOrderNumber: "006754",
+    //  amount: {currency: "PLN", value: "87.00"},
+    //  date: "2016-03-06T23:00:00.785Z",
+    //  special: true,
+    //  pointOfSale: "BEC12189710",
+    //  taxRegistrationNumber: "7861012333",
+    //  trade: ""
+    //};
+
+
 
     var authRequest = {
       email: "zz@wp.pl",
@@ -29,7 +45,7 @@ describe('update ticket client test', function () {
     };
 
     //authClient.authorizeUser(authRequest, options, function (err, jar) {
-    //  expect(jar).to.be.not.undefined;
+    //  //expect(jar).to.be.not.undefined;
     //
     //  updateTicketClient.updateLotteryTicket(jar, id, updateTicketRequest, options, function (err, data) {
     //    console.log(err, data);
